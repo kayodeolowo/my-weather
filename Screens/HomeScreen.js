4308c64075905d1a26d2432da4f212aac7cc4347
kayodeolowo <officialkayodeolowo@gmail.com>
2023-08-02 05:23:45 +0100

    Home

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,10 +1,26 @@
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text , Image, SafeAreaView } from 'react-native'
 
 const HomeScreen = () => {
   return (
-    <View>
-        <Text className="pt-20 text-green-800 "> Home screen </Text>
+    <View className="flex-1 relative">
+       <StatusBar style='light'/>
+       <Image blurRadius={70} source={require('../assets/images/bg.png')} 
+       className="absolute h-full w-full" />
+       
+
+       <SafeAreaView className="flex flex-1">
+        {/* search section */}
+
+        <View style={{height: '7%'}} className="mx-4 relative z-50">
+          <View className="flex-row justify-end items-center rounded-full"> 
+
+          </View>
+
+        </View>
+
+       </SafeAreaView>
     </View>
   )
 }
--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../Screens/HomeScreen';
+import HomeScreen from '../screens/HomeScreen';
 import { LogBox, Text, View } from 'react-native';
 
 const Stack = createNativeStackNavigator();
