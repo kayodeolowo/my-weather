89d8fbfd9e66db5290797bb719e09e94d5d71852
kayodeolowo <officialkayodeolowo@gmail.com>
2023-08-02 05:01:25 +0100

    initial setup

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View>
+        <Text className="pt-20 text-green-800 "> Home screen </Text>
+    </View>
+  )
+}
+
+export default HomeScreen
